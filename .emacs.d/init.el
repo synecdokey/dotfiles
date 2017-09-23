1ef4bf8104c2c6445099ae8b9bb718727359bd61
Kelsey Zapata <key@kelseyz.org>
2017-09-23 16:25:34 +1200

    feat(emacs): use a package manager
    `use-package` seems like the most popular/complete option. Removing
    `evil-mode` as to keep everything working while being atomic

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,13 +1,8 @@
+ ;; packaging 101
 (require 'package)
+(setq package-enable-at-startup nil)
 (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
 (package-initialize)
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (evil))))
-(require 'evil)
-(evil-mode 1)
 
+ ;; load config from org file
 (org-babel-load-file "~/.emacs.d/config.org")
