3d1542fd30f8c3a18be2d85ee5af2651551dff64
Kelsey Zapata <key@kelseyz.org>
2017-10-07 14:19:07 +1300

    feat(awesome): remove start menu
    I tend to prefer `mod+r` whenever I need to fire up something

--- awesome/.config/awesome/rc.lua
+++ awesome/.config/awesome/rc.lua
@@ -205,7 +205,6 @@ awful.screen.connect_for_each_screen(function(s)
         layout = wibox.layout.align.horizontal,
         { -- Left widgets
             layout = wibox.layout.fixed.horizontal,
-            mylauncher,
             s.mytaglist,
             s.mypromptbox,
         },
