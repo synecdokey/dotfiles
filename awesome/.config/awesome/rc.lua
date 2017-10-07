9cb85056bfe23bbc618be57d3c26aeb8498dd3dd
Kelsey Zapata <key@kelseyz.org>
2017-10-07 20:36:48 +1300

    cleanup(awesome): remove unused functionalities

--- awesome/.config/awesome/rc.lua
+++ awesome/.config/awesome/rc.lua
@@ -312,21 +312,7 @@ globalkeys = gears.table.join(
 
     -- Prompt
     awful.key({ modkey },            "r",     function () awful.screen.focused().mypromptbox:run() end,
-              {description = "run prompt", group = "launcher"}),
-
-    awful.key({ modkey }, "x",
-              function ()
-                  awful.prompt.run {
-                    prompt       = "Run Lua code: ",
-                    textbox      = awful.screen.focused().mypromptbox.widget,
-                    exe_callback = awful.util.eval,
-                    history_path = awful.util.get_cache_dir() .. "/history_eval"
-                  }
-              end,
-              {description = "lua execute prompt", group = "awesome"}),
-    -- Menubar
-    awful.key({ modkey }, "p", function() menubar.show() end,
-              {description = "show the menubar", group = "launcher"})
+              {description = "run prompt", group = "launcher"})
 )
 
 clientkeys = gears.table.join(
