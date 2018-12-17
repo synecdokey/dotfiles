f196053cf880d5d8e5b5947d3a9a727b72331ccb
Kelsey Zapata <key@kelseyz.org>
2018-12-17 16:52:15 +1300

    feat(gpg): add my key to ssh & git

--- fish/.config/fish/conf.d/abbrs.fish
+++ fish/.config/fish/conf.d/abbrs.fish
@@ -1,5 +1,6 @@
 if not set -q fish_initialised
     abbr -a vi nvim
     abbr -a vim nvim
+    abbr -a gpg gpg2
     set -U fish_initialised
 end
--- fish/.config/fish/conf.d/init.fish
+++ fish/.config/fish/conf.d/init.fish
@@ -1,5 +1,6 @@
 if not set -q fish_initial
   set -U fish_user_paths $fish_user_paths ~/.yarn/bin/
+  set -U SSH_AUTH_SOCK ~/.gnupg/S.gpg-agent.ssh
   set -U fish_initial
 end
 set -xg XDG_CONFIG_HOME ~/.config
