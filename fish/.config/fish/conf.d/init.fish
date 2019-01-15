9bed85db1d52da723d636aae4b717d8974617719
Kelsey Zapata <key@kelseyz.org>
2019-01-15 13:55:43 +1300

    fix(fish): use a portable way to get yarn bin path

--- fish/.config/fish/conf.d/init.fish
+++ fish/.config/fish/conf.d/init.fish
@@ -1,5 +1,5 @@
 if not set -q fish_initial
-  set -U fish_user_paths $fish_user_paths ~/.yarn/bin/
+  set -U fish_user_paths $fish_user_paths (yarn global bin)
   set -U SSH_AUTH_SOCK ~/.gnupg/S.gpg-agent.ssh
   set -U fish_initial
 end
