491b824b80d79b0db24cda2e381217d454743d8b
Emilia Zapata <emilia@springload.co.nz>
2020-07-22 11:41:30 +1200

    feat(fish): remove unused abbrs and add a binding

--- fish/.config/fish/conf.d/init.fish
+++ fish/.config/fish/conf.d/init.fish
@@ -3,7 +3,11 @@ if not set -q fish_initial
   set -U SSH_AUTH_SOCK ~/.gnupg/S.gpg-agent.ssh
   set -U fish_initial
 end
+
 set -xg XDG_CONFIG_HOME ~/.config
 set -xg VISUAL nvim
 set -xg FZF_DEFAULT_COMMAND 'rg --files --hidden'
 set -xg NVM_DIR ~/.nvm
+
+# Makes C^z go back to the background task (vim 99% of the time)
+bind \cz 'fg'
