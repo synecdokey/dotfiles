2dd25ad5e1243f7e959f21e9a10f75c1185adb1c
Emilia Zapata <emilia@springload.co.nz>
2020-08-03 16:46:23 +1200

    fix(nvim): don't look in .git
    I never change anything there while working on a project

--- fish/.config/fish/conf.d/init.fish
+++ fish/.config/fish/conf.d/init.fish
@@ -6,7 +6,7 @@ end
 
 set -xg XDG_CONFIG_HOME ~/.config
 set -xg VISUAL nvim
-set -xg FZF_DEFAULT_COMMAND 'rg --files --hidden'
+set -xg FZF_DEFAULT_COMMAND 'rg --files --hidden --glob "!.git/"'
 set -xg NVM_DIR ~/.nvm
 
 # Makes C^z go back to the background task (vim 99% of the time)
