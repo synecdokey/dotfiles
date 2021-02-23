180045cf25f08312d121c266ec2ebb8eb2586cf8
Emilia Zapata <emilia@springload.co.nz>
2021-02-23 17:44:12 +1300

    chore(emacs): remove configuration
    I really wanted to make it work, because .org is amazing, and the
    plugin system is fairly more advanced, but the truth is, I can't get
    used to how slow it feels, and how brittle package interactions can be.
    
    I did love the fact that you could do litterate programming, and some of
    the niceties around prose writing, but that wasn't enough for me.

--- emacs/.emacs.d/init.el
+++
@@ -1,8 +0,0 @@
- ;; packaging 101
-(require 'package)
-(setq package-enable-at-startup nil)
-(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
-(package-initialize)
-
- ;; load config from org file
-(org-babel-load-file "~/.emacs.d/config.org")
