358784dae27bcdfa1f94fe82c73fccc54c5d89f9
Emilia Zapata <zapata.contact@gmail.com>
2021-10-29 23:08:00 +1300

    feat(nvim): move to init.lua

---
+++ nvim/.config/nvim/init.lua
@@ -0,0 +1,15 @@
+-- Remove netrw's top banner
+vim.g.netrw_banner = 0
+
+-- True colours
+vim.o.termguicolors = true
+
+-- Set visible trailing and tab characters
+vim.opt.list = true
+vim.opt.listchars = { tab = '»·', trail = '·' }
+
+-- Load all plugins
+require('plugins')
+
+-- Set theme
+vim.cmd 'colorscheme spring-night'
