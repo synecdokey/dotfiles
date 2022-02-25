59c1f5f5b6cd96e213fcc67952c2b6f96797f325
Emilia Zapata <zapata.contact@gmail.com>
2022-02-26 00:22:39 +1300

    feat(nvim): improve statusline display

--- nvim/.config/nvim/plugin/lualine.lua
+++ nvim/.config/nvim/plugin/lualine.lua
@@ -1,11 +1,39 @@
 require('lualine').setup {
   sections = {
-    lualine_a = { {'mode', fmt = function(str) return str:sub(1,1) end } }
+    lualine_a = { {'mode', fmt = function() return ' ' end, padding = 0 } },
+    lualine_b = { 'branch', {
+      'diagnostics',
+      symbols = {error = ' ', warn = ' ', info = ' ', hint = ' '},
+    } },
+    lualine_c = { { 'filename', path = 1, symbols = {
+          modified = '',
+          readonly = '',
+    } } },
+    lualine_x = { {
+      function()
+        local msg = ''
+        local buf_ft = vim.api.nvim_buf_get_option(0, 'filetype')
+        local clients = vim.lsp.get_active_clients()
+        if next(clients) == nil then
+          return msg
+        end
+        for _, client in ipairs(clients) do
+          local filetypes = client.config.filetypes
+          if filetypes and vim.fn.index(filetypes, buf_ft) ~= -1 then
+            msg = msg .. ' ' .. client.name
+          end
+        end
+        return msg
+      end,
+      color = { fg = '#ffffff', gui = 'bold' }
+    } },
+    lualine_y = { },
+    lualine_z = { {'filetype', colored = true, icon_only = true } }
   },
   options = {
+    icons_enabled = false,
     component_separators = { },
     section_separators = { },
-    icons_enabled = false,
     theme = 'nord'
   }
 }
