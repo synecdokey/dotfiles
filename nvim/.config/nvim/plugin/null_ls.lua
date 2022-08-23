b5d0b3311812ebc830953f3b182142f34ab2d214
Emilia Zapata <zapata.contact@gmail.com>
2022-08-23 21:39:57 +1200

    feat(null_ls): move to nvim 0.8

--- nvim/.config/nvim/plugin/null_ls.lua
+++ nvim/.config/nvim/plugin/null_ls.lua
@@ -1,22 +1,24 @@
 local null_ls = require('null-ls')
+local augroup = vim.api.nvim_create_augroup('LspFormatting', {})
 
 null_ls.setup({
   sources = {
     null_ls.builtins.formatting.prettier.with({
       prefer_local = 'node_modules/.bin',
+      extra_filetypes = { 'astro' },
     }),
     require('null-ls').builtins.formatting.stylua,
   },
   on_attach = function(client, bufnr)
     if client.supports_method('textDocument/formatting') then
+      vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
       vim.api.nvim_create_autocmd('BufWritePre', {
         buffer = bufnr,
+        group = augroup,
         callback = function()
           vim.lsp.buf.format({
-            filter = function(clients)
-              return vim.tbl_filter(function(client)
-                return client.name ~= 'tsserver'
-              end, clients)
+            filter = function(client)
+              return client.name == 'null-ls'
             end,
             bufnr = bufnr,
           })
