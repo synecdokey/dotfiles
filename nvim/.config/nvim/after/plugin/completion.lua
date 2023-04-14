9ef1a4276ade08df41236506d3a33c62a289d319
Emilia Zapata <zapata.contact@gmail.com>
2023-04-14 14:11:37 -0400

    fix(nvim): update cmp to latest API

--- nvim/.config/nvim/after/plugin/completion.lua
+++ nvim/.config/nvim/after/plugin/completion.lua
@@ -106,9 +106,7 @@ local on_attach = function(client, bufnr)
 end
 
 -- Wire completion to the LSP
-local caps = require('cmp_nvim_lsp').update_capabilities(
-  vim.lsp.protocol.make_client_capabilities()
-)
+local caps = require('cmp_nvim_lsp').default_capabilities()
 
 local servers = { 'cssls', 'tsserver', 'tailwindcss', 'astro' }
 
