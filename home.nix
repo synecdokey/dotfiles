690a286cd9eb3917a0efbeb476550418604a7934
Emilia Zapata <zapata.contact@gmail.com>
2023-09-26 21:25:33 -0400

    chore(nix): tweak fish config
    Wasn't aware home manager was smart enough to remove leading spaces, but
    now I know

--- home.nix
+++ home.nix
@@ -33,21 +33,21 @@
   programs.fish = {
     enable = true;
     interactiveShellInit = ''
-set -Ux CARGO_HOME ~/.cache/cargo/
-set -Ux PNPM_HOME ~/.cache/pnpm/
-set -U fish_greeting
+      set -Ux CARGO_HOME ~/.cache/cargo/
+      set -Ux PNPM_HOME ~/.cache/pnpm/
+      set -U fish_greeting
 
-fish_add_path $PNPM_HOME
-fish_add_path $CARGO_HOME/bin/
+      fish_add_path $PNPM_HOME
+      fish_add_path $CARGO_HOME/bin/
 
-set -xg XDG_CONFIG_HOME ~/.config
-set -xg VISUAL nvim
+      set -xg XDG_CONFIG_HOME ~/.config
+      set -xg VISUAL nvim
 
-# Makes C^z go back to the background task (vim 99% of the time)
-bind \cz 'fg'
+      # Makes C^z go back to the background task (vim 99% of the time)
+      bind \cz 'fg'
 
-fnm env --use-on-cd | source
-starship init fish | source
+      fnm env --use-on-cd | source
+      starship init fish | source
     '';
   };
 }
