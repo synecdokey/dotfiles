2caaf81039b596437bd2e1fec3807d1ee478bbd3
Emilia Zapata <zapata.contact@gmail.com>
2023-10-07 00:34:56 -0400

    feat(nvim): clean up leader initialisation

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -4,8 +4,8 @@ vim.g.netrw_banner = 0
 -- Set leader, needs to be defined asap to avoid mapping anything to the default
 -- leader
 vim.g.mapleader = ' '
-vim.api.nvim_set_keymap('n', vim.g.mapleader, '', { noremap = true })
-vim.api.nvim_set_keymap('v', vim.g.mapleader, '', { noremap = true })
+-- Prevent default behaviour of space to move by one character like l
+vim.keymap.set({ 'n', 'v' }, vim.g.mapleader, '<nop>')
 
 -- Load all plugins
 require('plugins')
