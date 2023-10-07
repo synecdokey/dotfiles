22e169aab19ebe28e4d84c2f2a621808b3635e4c
Emilia Zapata <zapata.contact@gmail.com>
2023-10-06 22:40:15 -0400

    feat(wezterm): tweak it a bit more
    Tweak line height & remove the window shadow

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -1,15 +1,16 @@
 local wezterm = require("wezterm")
 
 return {
+	check_for_updates = false,
+	color_scheme = "terafox",
+	enable_tab_bar = false,
 	font = wezterm.font_with_fallback({
 		"Comic Code",
 		"nonicons",
 	}),
 	font_size = 15,
-	line_height = 1.2,
-	color_scheme = "terafox",
-	enable_tab_bar = false,
-	check_for_updates = false,
+	line_height = 1.4,
+	window_decorations = "RESIZE | MACOS_FORCE_DISABLE_SHADOW",
 	window_padding = {
 		left = 4,
 		right = 4,
