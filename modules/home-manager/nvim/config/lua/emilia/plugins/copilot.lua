852cd99aaad80e764dc4c2a379d0a289b2a3b323
Emilia Zapata <zapata.contact@gmail.com>
2024-03-29 17:46:27 -0400

    feat(nvim): remove some copilot weirdness

--- modules/home-manager/nvim/config/lua/emilia/plugins/copilot.lua
+++ modules/home-manager/nvim/config/lua/emilia/plugins/copilot.lua
@@ -20,4 +20,34 @@ return {
       markdown = true,
     },
   },
+  config = function(_, opts)
+    local cmp = require('cmp')
+    local copilot = require('copilot.suggestion')
+    local luasnip = require('luasnip')
+
+    require('copilot').setup(opts)
+
+    local function set_trigger(trigger)
+      if not trigger and copilot.is_visible() then
+        copilot.dismiss()
+      end
+      vim.b.copilot_suggestion_auto_trigger = trigger
+      vim.b.copilot_suggestion_hidden = not trigger
+    end
+
+    cmp.event:on('menu_opened', function()
+      set_trigger(false)
+    end)
+    cmp.event:on('menu_closed', function()
+      set_trigger(not luasnip.expand_or_locally_jumpable())
+    end)
+
+    vim.api.nvim_create_autocmd('User', {
+      desc = 'Disable Copilot inside snippets',
+      pattern = { 'LuasnipInsertNodeEnter', 'LuasnipInsertNodeLeave' },
+      callback = function()
+        set_trigger(not luasnip.expand_or_locally_jumpable())
+      end,
+    })
+  end,
 }
