7240b7bcfbe4e3c78118f4a4d231051d053c5a28
Emilia Zapata <zapata.contact@gmail.com>
2025-01-29 23:43:56 -0500

    feat(neovim): change completion plugin
    Still plenty of qualms about having to wire this manually, but it's not
    looking like neovim will ever provide that.

--- modules/home-manager/nvim/config/lua/emilia/plugins/lsp.lua
+++ modules/home-manager/nvim/config/lua/emilia/plugins/lsp.lua
@@ -22,9 +22,7 @@ return {
     event = { 'BufReadPre', 'BufNewFile' },
     config = function()
       local lspconf = require('lspconfig')
-      local caps = require('cmp_nvim_lsp').default_capabilities()
-
-      local servers = { 'cssls', 'tailwindcss', 'gleam', 'astro', 'nil_ls' }
+      local caps = require('blink.cmp').get_lsp_capabilities()
 
       lspconf.biome.setup({
         cmd = { 'pnpm', 'biome', 'lsp-proxy' },
@@ -32,6 +30,7 @@ return {
         single_file_support = true,
       })
 
+      local servers = { 'cssls', 'tailwindcss', 'gleam', 'astro', 'nil_ls' }
       for _, lsp in ipairs(servers) do
         lspconf[lsp].setup({
           on_attach = on_attach,
@@ -48,63 +47,43 @@ return {
 
   -- Completion
   {
-    'hrsh7th/nvim-cmp',
-    event = 'InsertEnter',
-    dependencies = {
-      'nvim-lspconfig',
-      'hrsh7th/cmp-nvim-lsp',
-      'hrsh7th/cmp-buffer',
-      { 'l3mon4d3/luasnip', event = 'InsertEnter' },
-      'saadparwaiz1/cmp_luasnip',
+    'saghen/blink.cmp',
+    -- optional: provides snippets for the snippet source
+    dependencies = 'rafamadriz/friendly-snippets',
+
+    -- use a release tag to download pre-built binaries
+    version = '*',
+    -- AND/OR build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
+    -- build = 'cargo build --release',
+    -- If you use nix, you can build from source using latest nightly rust with:
+    -- build = 'nix run .#build-plugin',
+
+    ---@module 'blink.cmp'
+    ---@type blink.cmp.Config
+    opts = {
+      -- 'default' for mappings similar to built-in completion
+      -- 'super-tab' for mappings similar to vscode (tab to accept, arrow keys to navigate)
+      -- 'enter' for mappings similar to 'super-tab' but with 'enter' to accept
+      -- See the full "keymap" documentation for information on defining your own keymap.
+      keymap = { preset = 'default' },
+
+      appearance = {
+        -- Sets the fallback highlight groups to nvim-cmp's highlight groups
+        -- Useful for when your theme doesn't support blink.cmp
+        -- Will be removed in a future release
+        use_nvim_cmp_as_default = true,
+        -- Set to 'mono' for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
+        -- Adjusts spacing to ensure icons are aligned
+        nerd_font_variant = 'mono',
+      },
+
+      -- Default list of enabled providers defined so that you can extend it
+      -- elsewhere in your config, without redefining it, due to `opts_extend`
+      sources = {
+        default = { 'lsp', 'path', 'snippets', 'buffer' },
+      },
     },
-    opts = function()
-      vim.o.completeopt = 'menuone,noselect,noinsert'
-      local luasnip = require('luasnip')
-      local cmp = require('cmp')
-
-      return {
-        snippet = {
-          expand = function(args)
-            luasnip.lsp_expand(args.body)
-          end,
-        },
-        mapping = cmp.mapping.preset.insert({
-          ['<C-d>'] = cmp.mapping.scroll_docs(-4),
-          ['<C-f>'] = cmp.mapping.scroll_docs(4),
-          ['<C-s>'] = cmp.mapping.complete(),
-          ['<CR>'] = cmp.mapping.confirm({
-            behavior = cmp.ConfirmBehavior.Replace,
-            select = true,
-          }),
-          ['<Tab>'] = cmp.mapping(function(fallback)
-            local copilot = require('copilot.suggestion')
-
-            if copilot.is_visible() then
-              copilot.accept()
-            elseif cmp.visible() then
-              cmp.select_next_item()
-            elseif luasnip.expand_or_jumpable() then
-              luasnip.expand_or_jump()
-            else
-              fallback()
-            end
-          end, { 'i', 's' }),
-          ['<S-Tab>'] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-              cmp.select_prev_item()
-            elseif luasnip.jumpable(-1) then
-              luasnip.jump(-1)
-            else
-              fallback()
-            end
-          end, { 'i', 's' }),
-        }),
-        sources = cmp.config.sources({
-          { name = 'nvim_lsp' },
-          { name = 'luasnip' },
-        }, { { name = 'buffer' } }),
-      }
-    end,
+    opts_extend = { 'sources.default' },
   },
 
   -- Better TS experience than typescript-language-server
