d92efe9f9b679454a952ce42b9344b7d50fb7de5
Emilia Zapata <zapata.contact@gmail.com>
2025-02-17 16:06:50 -0500

    feat(nvim): fix copilot-blink integration

--- modules/home-manager/nvim/config/lua/emilia/plugins/copilot.lua
+++ modules/home-manager/nvim/config/lua/emilia/plugins/copilot.lua
@@ -1,52 +1,14 @@
 return {
   'zbirenbaum/copilot.lua',
+  cmd = 'Copilot',
+  build = ':Copilot auth',
   event = 'InsertEnter',
   opts = {
+    suggestion = { enabled = false },
     panel = { enabled = false },
-    suggestion = {
-      auto_trigger = true,
-      keymap = {
-        -- Handled by cmp
-        accept = false,
-        accept_word = '<m-w>',
-        accept_line = '<m-l>',
-        next = '<m-n>',
-        prev = '<m-p>',
-        dismiss = '<m-d>',
-      },
-    },
     filetypes = {
-      -- Somehow this is disabled by default
       markdown = true,
+      help = true,
     },
   },
-  -- config = function(_, opts)
-  --   local copilot = require('copilot.suggestion')
-  --   local luasnip = require('luasnip')
-  --
-  --   require('copilot').setup(opts)
-  --
-  --   local function set_trigger(trigger)
-  --     if not trigger and copilot.is_visible() then
-  --       copilot.dismiss()
-  --     end
-  --     vim.b.copilot_suggestion_auto_trigger = trigger
-  --     vim.b.copilot_suggestion_hidden = not trigger
-  --   end
-  --
-  --   cmp.event:on('menu_opened', function()
-  --     set_trigger(false)
-  --   end)
-  --   cmp.event:on('menu_closed', function()
-  --     set_trigger(not luasnip.expand_or_locally_jumpable())
-  --   end)
-  --
-  --   vim.api.nvim_create_autocmd('User', {
-  --     desc = 'Disable Copilot inside snippets',
-  --     pattern = { 'LuasnipInsertNodeEnter', 'LuasnipInsertNodeLeave' },
-  --     callback = function()
-  --       set_trigger(not luasnip.expand_or_locally_jumpable())
-  --     end,
-  --   })
-  -- end,
 }
--- modules/home-manager/nvim/config/lua/emilia/plugins/lsp.lua
+++ modules/home-manager/nvim/config/lua/emilia/plugins/lsp.lua
@@ -27,7 +27,6 @@ return {
       lspconf.biome.setup({
         cmd = { 'pnpm', 'biome', 'lsp-proxy' },
         capabilities = caps,
-        single_file_support = true,
       })
 
       local servers = { 'cssls', 'tailwindcss', 'gleam', 'astro', 'nil_ls' }
@@ -48,39 +47,32 @@ return {
   -- Completion
   {
     'saghen/blink.cmp',
-    -- optional: provides snippets for the snippet source
-    dependencies = 'rafamadriz/friendly-snippets',
+    dependencies = {
+      'giuxtaposition/blink-cmp-copilot',
+      'rafamadriz/friendly-snippets',
+    },
 
-    -- use a release tag to download pre-built binaries
     version = '*',
-    -- AND/OR build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
-    -- build = 'cargo build --release',
-    -- If you use nix, you can build from source using latest nightly rust with:
-    -- build = 'nix run .#build-plugin',
-
-    ---@module 'blink.cmp'
-    ---@type blink.cmp.Config
     opts = {
-      -- 'default' for mappings similar to built-in completion
-      -- 'super-tab' for mappings similar to vscode (tab to accept, arrow keys to navigate)
-      -- 'enter' for mappings similar to 'super-tab' but with 'enter' to accept
-      -- See the full "keymap" documentation for information on defining your own keymap.
       keymap = { preset = 'default' },
 
       appearance = {
-        -- Sets the fallback highlight groups to nvim-cmp's highlight groups
-        -- Useful for when your theme doesn't support blink.cmp
-        -- Will be removed in a future release
         use_nvim_cmp_as_default = true,
-        -- Set to 'mono' for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
-        -- Adjusts spacing to ensure icons are aligned
         nerd_font_variant = 'mono',
+        kind_icons = {
+          Copilot = ' ',
+        },
       },
-
-      -- Default list of enabled providers defined so that you can extend it
-      -- elsewhere in your config, without redefining it, due to `opts_extend`
       sources = {
-        default = { 'lsp', 'path', 'snippets', 'buffer' },
+        default = { 'lsp', 'path', 'snippets', 'buffer', 'copilot' },
+        providers = {
+          copilot = {
+            name = 'copilot',
+            module = 'blink-cmp-copilot',
+            score_offset = 100,
+            async = true,
+          },
+        },
       },
     },
     opts_extend = { 'sources.default' },
