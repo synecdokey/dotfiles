e180a5a216fc24e7c421ce2af3ca341647e50e37
Emilia Zapata <zapata.contact@gmail.com>
2025-07-14 21:05:57 -0400

    feat(nvim): add tailwind-tools

---
+++ modules/home-manager/nvim/config/lua/emilia/plugins/tailwind-tools.lua
@@ -0,0 +1,10 @@
+return {
+  'luckasRanarison/tailwind-tools.nvim',
+  name = 'tailwind-tools',
+  build = ':UpdateRemotePlugins',
+  dependencies = {
+    'nvim-treesitter/nvim-treesitter',
+    'neovim/nvim-lspconfig', -- optional
+  },
+  opts = {},
+}
